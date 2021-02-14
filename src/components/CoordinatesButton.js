0b3a86414fa9d05673e7442fc424e232a4d20f27
Mohamad Tassieh <mohamad.bassam.tessieh@gmail.com>
2021-02-14 06:45:37 +0000

    Automatically backed up by Learn

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -5,12 +5,12 @@ class CoordinatesButton extends React.Component {
   buttonCoord = (event) => {
      this.props.onReceiveCoordinates([event.clientX, event.clientY]);
    }
-  
+
    render() {
      return (
      <button onClick={this.buttonCoord}></button>
      );
    }
  }
- 
+
  export default CoordinatesButton
